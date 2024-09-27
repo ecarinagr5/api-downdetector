b659aac3927ba1b9ba55b87c7eb3827909f07c75
Carina Gonzalez <carinagonzalez@Carinas-MacBook-Pro.local>
2024-09-27 16:03:47 -0600

    [C-002] data

--- index.js
+++ index.js
@@ -41,12 +41,15 @@ const generateToken = async () => {
 const generateSite = async () => {
   const token = await generateToken();
   const options = {
-    method: 'GET',
-    headers: {authorization: `Bearer ${token?.access_token}`}
+    method: "GET",
+    headers: { authorization: `Bearer ${token?.access_token}` },
   };
 
   try {
-    const response = await axios.get('https://downdetectorapi.com/v2/sites',options);
+    const response = await axios.get(
+      "https://downdetectorapi.com/v2/sites",
+      options
+    );
     console.log("generateSite", response?.data);
   } catch (error) {
     console.error("Error generating token:", error);
@@ -56,8 +59,8 @@ const generateSite = async () => {
 const getCompanies = async () => {
   const token = await generateToken();
   const options = {
-    method: 'GET',
-    headers: {authorization: `Bearer ${token?.access_token}`}
+    method: "GET",
+    headers: { authorization: `Bearer ${token?.access_token}` },
   };
   //generateSite()
   try {
@@ -65,9 +68,14 @@ const getCompanies = async () => {
     //const response = await axios.get('https://downdetectorapi.com/v2/sites/13/companies/events',options);
     //const response = await axios.get('https://downdetectorapi.com/v2/sites/13/companies',options); Null
 
-
-const response = await axios.get('https://downdetectorapi.com/v2/sites/13/companies?page_size=25&order_by=name&order_direction=asc',options); 
-
+    /*const response = await axios.get(
+      "https://downdetectorapi.com/v2/sites/13/companies?page_size=25&order_by=name&order_direction=asc",
+      options
+    );*/
+    const response = await axios.get(
+      "https://downdetectorapi.com/v2/companies",
+      options
+    );
     console.log("getCompanies", response?.data);
   } catch (error) {
     console.error("Error generating token:", error);
